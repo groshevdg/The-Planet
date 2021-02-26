fd7f19616bb9155c3c238331bec3a7166fa20e67
groshevdg <groshevdg@mail.ru>
2021-02-26 14:22:30 +0300

    setup crashlytics for android

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,22 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:the_planet/config/theme.dart';
 import 'package:the_planet/screens/auth/auth_screen.dart';
 import 'package:the_planet/screens/intro/intro_screen.dart';
 import 'package:the_planet/screens/main/main_screen.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  await _initCrashlytics();
   runApp(PlanetApp());
+}
+
+Future<void> _initCrashlytics() async {
+  FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
 }
 
 class PlanetApp extends StatelessWidget {
@@ -16,9 +24,9 @@ class PlanetApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       localizationsDelegates: [
-         //GlobalWidgetsLocalizations.delegate,
-        // GlobalCupertinoLocalizations.delegate,
-        // GlobalMaterialLocalizations.delegate
+         GlobalWidgetsLocalizations.delegate,
+         GlobalCupertinoLocalizations.delegate,
+         GlobalMaterialLocalizations.delegate
       ],
       supportedLocales: [
         const Locale("en", ""),
@@ -45,6 +53,9 @@ class AppStartRouteScreen extends StatelessWidget {
     return FutureBuilder(
         future: openScreen(),
         builder: (context, snapshot) {
+        FirebaseCrashlytics.instance.log("some log");  
+        FirebaseCrashlytics.instance.log("еще some log"); 
+        FirebaseCrashlytics.instance.crash();
         if (snapshot.hasData && snapshot.connectionState == ConnectionState.done) {
           return snapshot.data == 0 ? IntroScreen() : Container(color: Colors.green, width: 50, height: 50);
         }
